f043027ed1f26e01a08c7b17a53cc97d7fce34cd
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-02 22:05:47 +0530

    hooks commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Itachi', age: 28 },
-      { name: 'Kisame', age: 29 },
-      { name: 'Might Guy', age: 25 }
-    ]
-  }
+// with hooks
+const App = props => {
+    const [personState, setPersonState] = useState({
+      persons: [
+        { name: 'Itachi', age: 28 },
+        { name: 'Kisame', age: 29 },
+        { name: 'Might Guy', age: 25 }
+      ]
+    });
 
-  switchNameHandler = () => {
-    // console.log('was clicked!');
-    // DON'T DO THIS!
-    // this.state.persons[0].name = 'Sasuke'
-    this.setState(
-      { persons: [
+    const [otherState, setOtherState] = useState('some other value');
+    console.log(personState, otherState);
+
+    const switchNameHandler = () => {
+      setPersonState({ 
+        persons: [
           { name: 'Sasuke', age: 23 },
           { name: 'Kisame', age: 29 },
           { name: 'Might Guy', age: 25 }
         ]
-      }
-    )
-  }
+      });
+    }
 
-  render() {
     // JSX
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <button onClick={ this.switchNameHandler }>Switch Name</button>
-        <Person name = { this.state.persons[0].name } age = { this.state.persons[0].age }/>
-        <Person name = { this.state.persons[1].name } age = { this.state.persons[1].age }>Skill: Ninjutsu</Person>
-        <Person name = { this.state.persons[2].name } age = { this.state.persons[2].age }>Skill: Taijutsu</Person>
+        <button onClick={ switchNameHandler }>Switch Name</button>
+        <Person name = { personState.persons[0].name } age = { personState.persons[0].age }/>
+        <Person name = { personState.persons[1].name } age = { personState.persons[1].age }>Skill: Ninjutsu</Person>
+        <Person name = { personState.persons[2].name } age = { personState.persons[2].age }>Skill: Taijutsu</Person>
       </div>
     );
-  }
 }
 
 export default App;
+
+// state = {
+//   persons: [
+//     { name: 'Itachi', age: 28 },
+//     { name: 'Kisame', age: 29 },
+//     { name: 'Might Guy', age: 25 }
+//   ],
+//   otherState: 'some other value'
+// };
+
+// switchNameHandler = () => {
+//   // console.log('was clicked!');
+//   // DON'T DO THIS!
+//   // personState.state.persons[0].name = 'Sasuke'
+  // this.setState(
+  //   { persons: [
+  //       { name: 'Sasuke', age: 23 },
+  //       { name: 'Kisame', age: 29 },
+  //       { name: 'Might Guy', age: 25 }
+  //     ]
+  //   }
+  // )
+// };
