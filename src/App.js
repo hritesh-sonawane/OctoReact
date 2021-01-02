acf9dad5679191a21c6e1dcf7966f4160a2c1cc7
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-02 20:25:18 +0530

    state commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Itachi', age: 28 },
+      { name: 'Kisame', age: 29 },
+      { name: 'Might Guy', age: 25 }
+    ]
+  }
+
   render() {
     // JSX
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <Person name="Sasuke" age="20" />
-        <Person name="Naruto" age="20">Skill: Ninjutsu</Person>
-        <Person name="Neji" age="21" />
+        <button>Switch Name</button>
+        <Person name = { this.state.persons[0].name } age = { this.state.persons[0].age }/>
+        <Person name = { this.state.persons[1].name } age = { this.state.persons[1].age }>Skill: Ninjutsu</Person>
+        <Person name = { this.state.persons[2].name } age = { this.state.persons[2].age }>Skill: Taijutsu</Person>
       </div>
     );
   }
