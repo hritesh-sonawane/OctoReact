7b50a2d988d5a1075b78babe5bfbc0baedcd5eed
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-02 20:37:59 +0530

    setState commit

--- src/App.js
+++ src/App.js
@@ -11,13 +11,27 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    // console.log('was clicked!');
+    // DON'T DO THIS!
+    // this.state.persons[0].name = 'Sasuke'
+    this.setState(
+      { persons: [
+          { name: 'Sasuke', age: 23 },
+          { name: 'Kisame', age: 29 },
+          { name: 'Might Guy', age: 25 }
+        ]
+      }
+    )
+  }
+
   render() {
     // JSX
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={ this.switchNameHandler }>Switch Name</button>
         <Person name = { this.state.persons[0].name } age = { this.state.persons[0].age }/>
         <Person name = { this.state.persons[1].name } age = { this.state.persons[1].age }>Skill: Ninjutsu</Person>
         <Person name = { this.state.persons[2].name } age = { this.state.persons[2].age }>Skill: Taijutsu</Person>
