458496926e74cb80d8735cbeca038245d8d72157
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-03 12:09:07 +0530

    condtnl content render

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Kisame', age: 29 },
       { name: 'Might Guy', age: 25 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   };
 
   switchNameHandler = (newName) => {
@@ -33,7 +34,12 @@ class App extends Component {
         { name: event.target.value, age: 25 }
       ]
     })
-  }
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow })
+  };
 
   render() {
 
@@ -51,30 +57,36 @@ class App extends Component {
         <h1>Hi, I'm a React App!</h1>
         <button 
           style={ style }
-          onClick={ this.switchNameHandler.bind(this, 'Sasuke!') }
+          onClick={ this.togglePersonHandler }
         >
-          Switch Name
+          Toggle Ninjas
         </button>
         <br/>
         <br/>
-        <Person 
-          name = { this.state.persons[0].name } 
-          age = { this.state.persons[0].age }
-          click = { this.switchNameHandler.bind(this, 'Sakura!') }
-        />
-        <Person 
-          name = { this.state.persons[1].name } 
-          age = { this.state.persons[1].age }
-        >
-          Skill: Ninjutsu
-        </Person>
-        <Person 
-          name = { this.state.persons[2].name } 
-          age = { this.state.persons[2].age }
-          change = { this.nameChangeHandler }
-        >
-          Skill: Taijutsu
-        </Person>
+        { this.state.showPersons ? 
+          <div>
+            <Person 
+              name = { this.state.persons[0].name } 
+              age = { this.state.persons[0].age }
+              click = { this.switchNameHandler.bind(this, 'Sakura!') }
+            >
+              Skill: Genjutsu
+            </Person>
+            <Person 
+              name = { this.state.persons[1].name } 
+              age = { this.state.persons[1].age }
+            >
+              Skill: Ninjutsu
+            </Person>
+            <Person 
+              name = { this.state.persons[2].name } 
+              age = { this.state.persons[2].age }
+              change = { this.nameChangeHandler }
+            >
+              Skill: Taijutsu
+            </Person>
+          </div> : null
+        }
       </div>
     );
   }
