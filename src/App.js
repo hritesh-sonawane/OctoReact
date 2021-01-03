e91d5ddb4a1ec5aa1f8001d7a3267a9b3133ce3b
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-03 11:20:30 +0530

    practice v1 commit

--- src/App.js
+++ src/App.js
@@ -1,80 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
   state = {
-    persons: [
-      { name: 'Itachi', age: 28 },
-      { name: 'Kisame', age: 29 },
-      { name: 'Might Guy', age: 25 }
-    ],
-    otherState: 'some other value'
-  };
+    username: 'Schrute'
+  }
 
-  switchNameHandler = (newName) => {
-    // console.log('was clicked!');
-    // DON'T DO THIS!
-    // this.state.persons[0].name = 'Sasuke'
-    this.setState({ 
-      persons: [
-        { name: newName, age: 23 },
-        { name: 'Kisame', age: 29 },
-        { name: 'Might Guy', age: 25 }
-      ]
-    })
-  };
-
-  nameChangeHandler = (event) => {
-    this.setState({ 
-      persons: [
-        { name: 'Itachi', age: 23 },
-        { name: 'Kisame', age: 29 },
-        { name: event.target.value, age: 25 }
-      ]
+  usernameChangeHandler = (event) => {
+    this.setState({
+      username: event.target.value
     })
   }
 
   render() {
-
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     // JSX
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
-        <button 
-          style={ style }
-          onClick={ this.switchNameHandler.bind(this, 'Sasuke!') }
-        >
-          Switch Name
-        </button>
-        <br/>
-        <br/>
-        <Person 
-          name = { this.state.persons[0].name } 
-          age = { this.state.persons[0].age }
-          click = { this.switchNameHandler.bind(this, 'Sakura!') }
-        />
-        <Person 
-          name = { this.state.persons[1].name } 
-          age = { this.state.persons[1].age }
-        >
-          Skill: Ninjutsu
-        </Person>
-        <Person 
-          name = { this.state.persons[2].name } 
-          age = { this.state.persons[2].age }
-          change = { this.nameChangeHandler }
-        >
-          Skill: Taijutsu
-        </Person>
+        <UserInput changed={ this.usernameChangeHandler } currentName={ this.state.username } />
+        <UserOutput userName="Dwight" />
+        <UserOutput userName="K" />
+        <UserOutput userName={ this.state.username } />
       </div>
     );
   }
